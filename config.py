5adeed285388c15ed204ce33452243edbb6ff473
DjMixpur2 <djmixpur2@gmail.com>
2024-12-06 23:01:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8015124288:AAHer6UJIOn-m5FYtaLiQTsHzfKEbsAGRYM")
     # The Telegram API things
     API_ID = int(os.environ.get("API_ID", "12850056"))
     API_HASH = os.environ.get("API_HASH", "15564ec4a1a2cbef87c99a9aa9e40b34")
